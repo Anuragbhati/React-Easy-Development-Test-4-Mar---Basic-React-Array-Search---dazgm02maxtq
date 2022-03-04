4d2fceb4ecc49ce1603b9bf4764db0c14d080521
Anuragbhati <91306154+Anuragbhati@users.noreply.github.com>
2022-03-04 19:21:52 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,1 +1,30 @@
+import React from 'react'
+import {useState} from 'react'
 
+function Search(props) {
+    const [item, setItem]=useState([])
+    const [data,setData]=useState("")
+const handleChange=(e)=>{
+   let data=e.target.value;
+   setData()
+   console.log(data)
+}
+  setItem([props.array]);
+
+
+  return (
+    <div>
+        <h3>Search</h3>
+        <form>
+            <input type={"text"} onChange={handleChange}></input>
+        </form>
+        {
+         item.map((res)=>{
+             return !data?null:<ol><li>{}</li></ol> 
+         })
+        }
+    </div>
+  )
+}
+
+export default Search;
