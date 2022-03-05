d90ad630bb6f65c6edfa9f6624cbdb200d958a5d
Anuragbhati <91306154+Anuragbhati@users.noreply.github.com>
2022-03-06 00:01:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import Search from './Search'
+import '../styles/App.css';
+import Search from "./Search"
 
-// import '../styles/App.css';
-
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
 
   
   return (
     <div id="main">
-      <Search array={searchArray} ></Search>
+      <Search></Search>
     </div>
   )
 }
